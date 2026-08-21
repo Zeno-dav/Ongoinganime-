3c363877814fd21858dc7c6662458e5923465a53
Zeno-dav <growzenosmm@gmail.com>
2026-08-21 08:55:48 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -26,7 +26,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "✅ Ongoing Anime Multi-Bot Cluster is Active & Running 24/7
+    return "✅ Ongoing Anime Multi-Bot Cluster is Active & Running 24/7"
 
 def run_flask_server():
     port = int(os.environ.get("PORT", 8080))
